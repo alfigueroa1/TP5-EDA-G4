c50d4e7c881aa94c3039a459160e3eecfa4f8509
MTripo <mtripoli@iba.edu.ar>
2019-09-17 16:34:12 -0300

    new FSM

--- wormFSM.cpp
+++ wormFSM.cpp
@@ -1,15 +1,14 @@
-#pragma once
-#include "wormFSM.h"
-
-wormFSM :: wormFSM(const cellType* const table = NULL, const unsigned int rows = 0, const unsigned int columns = 0, stateTypes initState = 0)
-	: state(initState), rowCount(rows), columnCount(columns), pTableFSM(table) {}
-void wormFSM::nothing(void){}
-
-void wormFSM::next(int event)
-{
-	temp = pTableFSM[static_cast<unsigned int>(state) * columnCount + event];
-	auto f = bind(temp.action, this);
-	f();
-	state = pTableFSM[state * columnCount + event].nextState;
-}
-
+//#pragma once
+//#include "wormFSM.h"
+//
+//wormFSM :: wormFSM(const cellType* const table = NULL, const unsigned int rows = 0, const unsigned int columns = 0, stateTypes initState = 0, double pos_x = 701, double pos_y = 616)
+//	: state(initState), rowCount(rows), columnCount(columns), pTableFSM(table), pos_x(pos_x), pos_y(pos_y) {}
+//
+//void wormFSM::next(int event)
+//{
+//	temp = pTableFSM[static_cast<unsigned int>(state) * columnCount + event];
+//	auto f = bind(temp.action, this);
+//	f();
+//	state = pTableFSM[state * columnCount + event].nextState;
+//}
+//
--- wormFSM.h
+++ wormFSM.h
@@ -1,32 +1,33 @@
-#pragma once
-#include <functional>
-#include <iostream>
-#include <new>
-
-using namespace std;
-using namespace std::placeholders;
-using stateTypes = unsigned int;
-
-class wormFSM;
-
-typedef struct
-{
-	stateTypes nextState;
-	void (wormFSM::* action) (void);
-}cellType;
-
-
-class wormFSM
-{
-public:
-	wormFSM(const cellType* const table, const unsigned int rows, const unsigned int columns, stateTypes initState);
-	void next(int event);
-
-protected:
-	void nothing(void);
-	cellType temp;
-	const unsigned int rowCount;
-	const unsigned int columnCount;
-	const cellType* const pTableFSM;
-	stateTypes state;
-};
+//#pragma once
+//#include <functional>
+//#include <iostream>
+//#include <new>
+//
+//using namespace std;
+//using namespace std::placeholders;
+//using stateTypes = unsigned int;
+//
+//class wormFSM;
+//
+//typedef struct
+//{
+//	stateTypes nextState;
+//	void (wormFSM::* action) (void);
+//}cellType;
+//
+//
+//class wormFSM
+//{
+//public:
+//	wormFSM(const cellType* const table, const unsigned int rows, const unsigned int columns, stateTypes initState, double pos_x, double pos_y);
+//	void next(int event);
+//
+//protected:
+//	double pos_x;
+//	double pos_y;
+//	cellType temp;
+//	const unsigned int rowCount;
+//	const unsigned int columnCount;
+//	const cellType* const pTableFSM;
+//	stateTypes state;
+//};
